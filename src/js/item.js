6f289bbdfefc352c5a13d8222f48695941d73b08
Samuel@CERULEAN <keroame@hotmail.com>
2021-05-12 01:25:23 +0800

    convert to OOP style

--- src/js/item.js
+++ src/js/item.js
@@ -1,6 +1,47 @@
-function rate()
-{
-    var result = document.getElementById('result');
-    result.style.display='block';
-    result.innerHTML = 'Your artifact is trash.';
+var arr;
+
+function init() {
+    arr = new itemArray();
+}
+
+class itemArray {
+    constructor() {
+        this.data = [];
+        if (!this.data.length) {
+            this.data.push(new item(this.data.length));
+            this.data.push(new item(this.data.length));
+        }
+    }
+
+    rateRedir(id) {
+        this.data[id].rateImpl();
+    }
+}
+
+class item {
+    constructor(id) {
+        this.id = id;
+        this.createHTML(id);
+    }
+
+    createHTML(id) {
+        // read from template block
+        this.element = document.createElement("div");
+        this.element.id = "block" + id;
+        this.element.className = "datablock";
+        this.element.innerHTML = document.getElementById("datablockTemplate").innerHTML;
+        this.element.innerHTML = this.element.innerHTML.replace("rate(-1)", "rate(" + id + ")");
+        this.element.innerHTML = this.element.innerHTML.replace("rst", "rst" + id);
+        document.getElementById("grid-container").appendChild(this.element);
+    }
+
+    rateImpl() {
+        var result = document.getElementById("rst" + this.id);
+        result.style.display='block';
+        result.innerHTML = evaluate(this.element);
+    }
+}
+
+function rate(id) {
+    arr.rateRedir(id);
 }
---
+++ src/js/rater.js
@@ -0,0 +1,5 @@
+
+function evaluate(el) {
+    
+    return el.querySelector("#tss1").value + " " + el.querySelector("#tss2").value + " " + el.querySelector("#tss3").value + " " + el.querySelector("#tss4").value + " " + el.querySelector("#tss5").value;
+}
