4a1a64092ce3aaa6971c792eda955c4611162cc2
Samuel@CERULEAN <keroame@hotmail.com>
2021-05-21 02:50:32 +0800

    v0.04 - determine value of +0 artifact

--- src/js/rater.js
+++ src/js/rater.js
@@ -1,6 +1,54 @@
+var color_red = "rgb(255, 190, 184)";
+var color_yellow = "rgb(255, 241, 184)";
+var color_green = "rgb(184, 255, 186)";
+var min_stat_values = [0, 14, 4.1, 5.4, 2.7, 16, 5.1, 16, 4.5, 209, 4.1];
+var max_stat_values = [0, 19, 5.8, 7.8, 3.9, 23, 7.3, 23, 6.5, 299, 5.8];
+var stat_diffs = [0, 5, 1.7, 2.4, 1.2, 7, 2.2, 7, 2, 90, 1.7];
 
 function evaluate(el) {
     
-    return "0.00";
-    //return el.querySelector("#tss1").value + " " + el.querySelector("#tss2").value + " " + el.querySelector("#tss3").value + " " + el.querySelector("#tss4").value + " " + el.querySelector("#tss5").value;
+    // make bitset of desired subsets 
+    let desired = 0x00, ratingSum = 0x00, ratingIdv = 0x00, stat_allocs = 4;
+    var i = 1;
+    for (; i < 6; ++i)
+        desired |= 0x01 << el.querySelector("#tss" + i).value;
+    desired &= ~(0x01 << 0);    // clear last bit (NONE bit)
+
+    try
+    {
+        // loop through each substat
+        for (i = 1; i < 5; ++i)
+        {
+            // determine if this substat should be included
+            if (!(desired & (0x01 << el.querySelector("#ss" + i).value)))
+            {
+                el.querySelector("#ss" + i).style.backgroundColor = color_yellow;
+                el.querySelector("#iss" + i).style.backgroundColor = color_yellow;
+                continue;
+            }
+
+            // check for valid range
+            let stat_index = el.querySelector("#ss" + i).value;
+            let stat_value = el.querySelector("#iss" + i).value;
+            if (stat_value < min_stat_values[stat_index] || stat_value > max_stat_values[stat_index] * 6)
+            {
+                el.querySelector("#iss" + i).style.backgroundColor = color_red;
+                throw "Invalid Stat";
+            }
+
+            // valid range, do value processing here
+            el.querySelector("#ss" + i).style.backgroundColor = color_green;
+            el.querySelector("#iss" + i).style.backgroundColor = color_green;
+            ratingIdv = (stat_value - min_stat_values[stat_index]) / stat_diffs[stat_index];
+
+            ratingSum += ratingIdv;
+        }
+    }
+    catch (err)
+    {
+        ratingSum = err;
+    }
+    
+
+    return Math.round(ratingSum / stat_allocs * 10000) / 100 + "%";
 }
